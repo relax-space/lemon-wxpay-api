fc650bcfdb1391f6104e6bb272ae11fdd17bd8f2
xiao.xinmiao <xiaoxm_001@163.com>
2017-11-09 15:23:34 +0800

    move flag into global variable

--- green.go
+++ green.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/relax-space/go-kit/model"
 
@@ -123,12 +121,6 @@ func ReverseGreen(c echo.Context) error {
 
 func Account() greenAccount {
 
-	appId := flag.String("WXPAY_APPID", os.Getenv("WXPAY_APPID"), "WXPAY_APPID")
-	key := flag.String("WXPAY_KEY", os.Getenv("WXPAY_KEY"), "WXPAY_KEY")
-	mchId := flag.String("WXPAY_MCHID", os.Getenv("WXPAY_MCHID"), "WXPAY_MCHID")
-	certName := flag.String("CERT_NAME", os.Getenv("CERT_NAME"), "CERT_NAME")
-	certKey := flag.String("CERT_KEY", os.Getenv("CERT_KEY"), "CERT_KEY")
-	rootCa := flag.String("ROOT_CA", os.Getenv("ROOT_CA"), "ROOT_CA")
 	account := greenAccount{AppId: *appId, Key: *key, MchId: *mchId,
 		CertPathName: *certName, CertPathKey: *certKey, RootCa: *rootCa,
 	}
--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"flag"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+)
+
+var (
+	appId    = flag.String("WXPAY_APPID", os.Getenv("WXPAY_APPID"), "WXPAY_APPID")
+	key      = flag.String("WXPAY_KEY", os.Getenv("WXPAY_KEY"), "WXPAY_KEY")
+	mchId    = flag.String("WXPAY_MCHID", os.Getenv("WXPAY_MCHID"), "WXPAY_MCHID")
+	certName = flag.String("CERT_NAME", os.Getenv("CERT_NAME"), "CERT_NAME")
+	certKey  = flag.String("CERT_KEY", os.Getenv("CERT_KEY"), "CERT_KEY")
+	rootCa   = flag.String("ROOT_CA", os.Getenv("ROOT_CA"), "ROOT_CA")
 )
 
 func main() {
